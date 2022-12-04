807862867536724b27a83260910bbd6110c555b1
Luan16p <88367567+Luan16p@users.noreply.github.com>
2022-12-04 18:14:11 -0400

    3 files modified, 1 added, 3 deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
+import * as React from 'react';
 import './App.css';
 
-import './Css/Responsive.css'
-import { BrowserRouter } from 'react-router-dom'
+import './Css/Responsive.css';
+import { BrowserRouter } from 'react-router-dom';
 import Header from './Components/Header';
 import Carousel_ from './Components/Carousel_';
 import Content from './Components/Content';
 import Footer from './Components/Footer';
 
 export default function App() {
-
-  
-
   return (
     <div className="App">
       <BrowserRouter>
@@ -21,7 +18,7 @@ export default function App() {
           <Content />
         </div>
 
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import * as React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
